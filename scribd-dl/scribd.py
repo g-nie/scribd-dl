335b8a508d13ca96a4e6400fc1694fedb404ffac
Giannis Terzopoulos <terzo.giannis@gmail.com>
2018-03-09 14:26:05 +0200

    Formatted

--- scribd-dl/scribd.py
+++ scribd-dl/scribd.py
@@ -1,8 +1,6 @@
-import time
 import re
 import sys
 import os
-import traceback
 import argparse
 import logging
 from ast import literal_eval
@@ -19,13 +17,6 @@ LOAD_TIME = 20  # Stop loading page after 20 page
 logfolder = './logs/'
 if not os.path.exists(logfolder):
     os.makedirs(logfolder)
-
-
-# # def read_images(rawdata, colorspace, first_frame_only=False):
-# def read_images(*args, **kwargs):
-#     logging.getLogger().setLevel(logging.INFO)
-#     read_images(*args, **kwargs)
-
 
 
 def valid_url(u):
@@ -60,13 +51,11 @@ args = parser.parse_args()
 url = args.url
 log_level = logging.DEBUG if args.verbose else logging.INFO
 
-# TODO : WORS BUT WITH img2pdf DEBUG OUTPUT IN .LOG + DEVTOOLS...
-
 # Initialize and set up the logging system
 url_id = re.search(r'(?P<id>\d+)', url).group('id')
 logging.basicConfig(level=logging.DEBUG,
-                    # format='%(levelname)s [%(asctime)s] [{}]  %(message)s'.format(url_id),
-                    format='%(module)s - %(name)s %(levelname)s [%(asctime)s] [{}]  %(message)s'.format(url_id),
+                    format='%(levelname)s [%(asctime)s] [{}]  %(message)s'.format(url_id),
+                    # format='%(module)s - %(name)s %(levelname)s [%(asctime)s] [{}]  %(message)s'.format(url_id),
                     datefmt='%d-%m-%Y %H:%M:%S',
                     filename='{}scribd.log'.format(logfolder),
                     filemode='w')
@@ -77,48 +66,25 @@ console_handler.setLevel(log_level)
 logging.getLogger().addHandler(console_handler)
 logger = logging.getLogger('scribd')
 
+
 # Silence unnecessary third party debug messages
 logging.getLogger('selenium.webdriver.remote.remote_connection').setLevel(logging.INFO)
 logging.getLogger('PIL.PngImagePlugin').setLevel(logging.INFO)
 logging.getLogger('PIL.Image').setLevel(logging.INFO)
-logging.getLogger('img2pdf').setLevel(logging.INFO)  # --------
+# logging.getLogger('img2pdf').setLevel(logging.INFO)  # --------
 
-# logging.getLogger('remote_connection').setLevel(logging.INFO)
-# logging.getLogger('Image').setLevel(logging.INFO)
-# logging.getLogger('img2pdf').setLevel(logging.INFO)
-# logging.getLogger('PngImagePlugin').setLevel(logging.INFO)
-
-
-# user_agent = "Mozilla/5.0 (Linux; Android 4.4.2; ASUS_T00J Build/KVT49L) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/30.0.0.0 Safari/537.36"
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36"
 options = webdriver.ChromeOptions()
-options.add_argument('--user-agent={}'.format(user_agent))
+# options.add_argument('--user-agent={}'.format(user_agent))
 options.add_argument('--headless')
 options.add_argument('--log-level=3')
 options.add_argument('--disable-logging')
 options.add_argument('--disable-gpu')
 options.add_argument('--disable-infobars')
-# options.add_argument("--start-maximized")  # OXI STO HEADLESS
-options.add_argument("--window-size=1600,2020")  # STO HEADLESS
+options.add_argument("--window-size=1600,2020")
 driver = webdriver.Chrome(options=options, service_args=["--log-path=chromedriver.log"])
 
-
-# ---------- FOR DEBUG
-def excepthook(*exc_info):
-    driver.quit()
-    traceback.print_exception(*exc_info)
-    print('An unexpected error occured, please try again')
-
-
-sys.excepthook = excepthook
-# print()
-
-# url = 'https://www.scribd.com/document/106884805/Nebraska-Wing-Sep-2012'  # 16 pages
-# url = 'https://www.scribd.com/doc/18587980/ARXAIA-G-Gymnasioy'  # 456 pages
-# url = 'https://www.scribd.com/document/294632720/Connecticut-Wing-Apr-2014'  # 21 pages
-# url = 'https://www.scribd.com/document/128090739/The-Bedan-Journal-of-Psychology-2013'
-# url = 'https://www.scribd.com/document/90403141/Social-Media-Strategy'  # 22 pages
-
+logger.info('Visiting requested url')
 driver.set_page_load_timeout(LOAD_TIME)
 try:
     driver.get(url)
@@ -169,7 +135,6 @@ for counter in range(1, int(total_pages) + 1):
     if counter < first_page:
         continue
     processed += 1
-    # print('Processing page : {} of {}'.format(counter, last_page), end='\r')
     logger.debug('Processing page : %s of %s', counter, last_page)
 
     location = page.location
@@ -195,18 +160,29 @@ for counter in range(1, int(total_pages) + 1):
         Pages.clear()
         chunk_counter += 1
 
+driver.quit()
+
 merger = PdfFileMerger()
 for pdf in Tmp_files:
     merger.append(pdf)
 title = re.sub('[^\w\-_\.\,\!\(\)\[\]\{\}\;\'\΄ ]', '_', title)
 drive = os.path.abspath(os.sep)
-path = '{}Users\\{}\\Desktop\\{}.pdf'.format(drive, os.getlogin(), title)  # Change -----
-# path = '{}.pdf'.format(title)
-merger.write(path)
-# merger.write(f'{title}.pdf')
+# path = '{}Users\\{}\\Desktop\\{}.pdf'.format(drive, os.getlogin(), title)  # Change -----
+path = '{}.pdf'.format(title)
+# merger.write(path)
+merger.write(f'{title}.pdf')
 merger.close()
+
 logger.debug('Sucessfully downloaded : %s', path)
 for pdf in Tmp_files:
     os.remove(pdf)
 
-driver.quit()
+# ---------- USE FOR DEBUGGING UNCAUGHT EXCEPTIONS
+# def excepthook(*exc_info):
+#     driver.quit()
+#     traceback.print_exception(*exc_info)
+#     print('An unexpected error occured, please try again')
+# sys.excepthook = excepthook
+
+# TODO : Silence DEVTOOLS...
+# TODO : logging.getLogger('img2pdf').setLevel(logging.INFO) not working
