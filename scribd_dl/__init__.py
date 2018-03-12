ca9b2cd2a7d17b6c52215ad8304f6053d57c8b1c
Giannis Terzopoulos <terzo.giannis@gmail.com>
2018-03-12 16:36:23 +0200

    Edited __init__.py

--- scribd_dl/__init__.py
+++ scribd_dl/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# from .scribd_dl import ScribdDL
-__all__ = ['scribd_dl']
+# __all__ = ['.scribd_dl']
+from .scribd_dl import ScribdDL  # noqa: F401
 
 __author__ = """Giannis Terzopoulos"""
 __email__ = 'terzo.giannis@gmail.com'
