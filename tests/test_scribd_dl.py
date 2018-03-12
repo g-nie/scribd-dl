0499997593b48f97370944d23e50b6f894ed1898
Giannis Terzopoulos <terzo.giannis@gmail.com>
2018-03-12 14:25:05 +0200

    Edited test_scribd_dl.py

---
+++ tests/test_scribd_dl.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import unittest
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from scribd_dl import scribd_dl
+
+
+class TestScribd_dl(unittest.TestCase):
+    """Tests for `scribd_dl` package."""
+
+    # def setUp(self):
+    #     """Set up test fixtures, if any."""
+
+    # def tearDown(self):
+    #     """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        t1 = 'test text 1'
+        t2 = 'test text 2'
+        self.assertEqual(t1, t2)
+
+
+if __name__ == '__main__':
+    unittest.main()
