99d78b6b1d89d5551a6a97e85657de1add6dc1f1
Giannis Terzopoulos <terzo.giannis@gmail.com>
2018-03-23 19:08:18 +0200

    version 0.4.3

--- scribd_dl/version.py
+++ scribd_dl/version.py
@@ -4,7 +4,7 @@
 AUTHOR = "Giannis Terzopoulos"
 EMAIL = 'terzo.giannis@gmail.com'
 STATUS = 'Development Status :: 4 - Beta'
-VERSION = '0.4.2'
+VERSION = '0.4.3'
 DATE = '2018 March'
 
 # Development Status :: 1 - Planning
